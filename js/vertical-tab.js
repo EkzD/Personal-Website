510d616b9db659035be67c6d9e24efb4353ea4d9
Syris <osiriskedigadashpalac@gmai;.com>
2025-11-08 16:59:26 +0800

    Added footer to the webpages, Finished up the education webpage. Made a functional sidebar for the interest page and possibly for the project page as well

---
+++ js/vertical-tab.js
@@ -0,0 +1,22 @@
+function openTab(evt, tabName) {
+  var tabcontent = document.getElementsByClassName("tabcontent");
+  for (var i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  var tablinks = document.getElementsByClassName("tablinks");
+  for (var i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(tabName).style.display = "block";
+
+  evt.currentTarget.className += " active";
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  var firstTab = document.querySelector(".tablinks");
+  if (firstTab) {
+    firstTab.click();
+  }
+});
